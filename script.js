73b7b1fd555d5f60b4775f368a2b75eda35fa7ea
Markley Douglas Mourão de Sales <marksales.dev.eng@gmail.com>
2021-12-16 05:08:02 -0300

    attributeinlet

---
+++ script.js
@@ -0,0 +1,11 @@
+alert("Bem-vindo! a seguir, pediremos alguns dados.");
+
+let nameUser= prompt("Qual seu nome?");
+
+let age= prompt("Qual sua idade?");
+
+let ageConfirm= confirm("Sua idade é " + age + "?");
+
+alert("Seu nome é " + nameUser);
+alert("Sua idade é " + age);
+alert("Idade confirmada: " + ageConfirm);
